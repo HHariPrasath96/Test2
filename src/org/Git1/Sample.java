b205dd68119eced9f6c4fd2e517f91677259a129
HHariprasath96 <hariprasathalagesan@gmail.com>
2022-11-08 21:20:26 +0530

    Test8

---
+++ src/org/Git1/Sample.java
@@ -0,0 +1,37 @@
+package org.Git1;
+
+public class Sample {
+	
+	private void method1() {
+        System.out.println("c#");
+	}
+	
+	
+	private void method2() {
+    System.out.println("selenium");
+	}
+	
+	
+	private void method3() {
+         System.out.println("testing");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
